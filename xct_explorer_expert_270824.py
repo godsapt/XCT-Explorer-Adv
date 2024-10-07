b1d9b8470294d98fa9a56649027fc2b461f5fc7a
Jose Godinho <56164560+godsapt@users.noreply.github.com>
2024-10-07 13:03:25 +0200

    Update xct_explorer_expert_270824.py
    link to the publication

--- xct_explorer_expert_270824.py
+++ xct_explorer_expert_270824.py
@@ -11,7 +11,7 @@ tabCitation, tabInstructions, tabGeometry, tabComposition, tabSummary= st.tabs([
 with tabCitation:
     st.write('The XCT-Explorer-Advanced is a graphic user interface designed to be an intuitive and interactive tool to help planning CT experiments. New users are advised to use the simplified version of this app https://xct-explorer-v1.streamlit.app/. Note that the advanced features are experimental')
     st.subheader ('Citation')
-    st.write ('If you find XCT-explorer useful for the planning of your experiment please cite the publication :blue[?? TMS 2024]. The advanced features were not peer-reviewed') 
+    st.write ('If you find XCT-explorer useful for the planning of your experiment please cite the publication :blue[https://doi.org/10.1016/j.tmater.2024.100041]. The advanced features were not peer-reviewed') 
     st.write ('Advanced features: possibility to add phases that are not in the database, select the filter composition, manualy adjust the filter thickness, export the list of parameters')
     st.subheader('Important disclaimer')
     st.write ('Always interpret the results critically and discuss your assessment with a CT expert')
